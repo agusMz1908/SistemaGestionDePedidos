f8c9e1082712d24c21b14ce83c78c36cf37f7b44
Agustin Muniz <agus.muniz1997@gmail.com>
2023-07-31 20:18:31 -0300

    Agregar archivos de proyecto.

---
+++ ContadorHandy.Modelos/Pedido.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContadorHandy.Modelos
+{
+    public enum TipoPedido
+    { 
+        Normal,
+        PosLink
+    }
+    public class Pedido
+    {
+       [Key]
+        public int Id { get; set; }
+        public int Numero { get; set; }
+        public TipoPedido Tipo { get; set; }
+        public int EquiposETH { get; set; }
+        public int Equipos3G { get; set; }
+        public int Total => EquiposETH + Equipos3G;
+        public int Pendientes => Total - (EquiposETH + Equipos3G);
+        public int EntregadosETH { get; set; }
+        public int Entregados3G { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public DateTime FechaFinalizado { get; set; }
+
+    }
+}
---
+++ ContadorHandy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ContadorHandy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
