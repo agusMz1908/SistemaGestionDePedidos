65c3886f6154ba0995412a5848f10a93a60d0102
Agustin Muniz <amuniz.1908@outlook.com>
2023-08-14 23:02:14 -0300

    Add User Roles and smalls changes in UI.

---
+++ ContadorHandy.Utilidades/EmailSender.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContadorHandy.Utilidades
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            //Logica para enviar el mail.
+            return Task.CompletedTask;
+        }
+    }
+}
--- ContadorHandy/Program.cs
+++ ContadorHandy/Program.cs
@@ -3,6 +3,9 @@ using ContadorHandy.AccesoDatos.Repository.IRepository;
 using ContadorHandy.AccesoDatos.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using ContadorHandy.Utilidades;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,10 +14,11 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ConexionSQL")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IPedidoRepository, PedidoRepository>();
+builder.Services.AddScoped<IEmailSender, EmailSender>();
 
 var app = builder.Build();
 
